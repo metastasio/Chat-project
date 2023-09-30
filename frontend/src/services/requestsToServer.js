7d46c785f5ea180244935e78154de74a90d551d0
metastasio <myaldzina.m@gmail.com>
2023-09-30 22:49:03 +0300

    Added API routes

--- frontend/src/services/requestsToServer.js
+++ frontend/src/services/requestsToServer.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
 
-const axiosConfig = { timeout: 5000 };
-const getUserToken = (userData) => axios.post('/api/v1/login', userData, axiosConfig);
+const routes = {
+  loginPath: () => '/api/v1/login',
+  signupPath: () => '/api/v1/signup',
+  contentPath: () => '/api/v1/data',
+};
 
-const getChatContent = (token) => axios.get('/api/v1/data', {
+const axiosConfig = { timeout: 5000 };
+const getUserToken = (userData) => axios.post(routes.loginPath(), userData, axiosConfig);
+
+const getChatContent = (token) => axios.get(routes.contentPath(), {
   headers: {
     Authorization: `Bearer ${token}`,
   },
 }, axiosConfig);
 
-const createNewUser = ({ username, password }) => axios.post('/api/v1/signup', { username, password }, axiosConfig);
+const createNewUser = ({ username, password }) => axios
+  .post(routes.signupPath(), { username, password }, axiosConfig);
 
 export { getUserToken, getChatContent, createNewUser };
